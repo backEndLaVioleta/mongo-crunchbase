c1652d56fa3fcf8eeca383670506fc67ef406a3c
naranjito72 <theclick36@gmail.com>
2021-11-05 08:31:14 +0100

    readline

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import connection from './database.js';
 import queries from './queries.js';
 // readline
+import readline from 'readline';
+import clear from 'clear';
+import { stdin as input, stdout as output} from 'process';
 
-console.log(connection);
+
+const rl = readline.createInterface({input, output});
+
 // connection.findInCollection('companies', {}).then(console.log).catch(console.error);
 connection.countDb('companies').then(console.log).catch(console.error);
 console.log(queries[1])
